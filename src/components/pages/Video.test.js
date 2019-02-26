670763b12b66c752e9cf9b2ef45d9dfe68744d6e
Gavin Cockrem <gavin.cockrem@joipolloi.com>
2019-02-26 11:40:16 +0000

    change data used to test video

--- src/components/pages/Video.test.js
+++ src/components/pages/Video.test.js
@@ -5,7 +5,7 @@ import { shallow } from 'enzyme';
 import Video from './Video';
 import data from '../../../public/manifest';
 
-const pageData = data.content.contents[1];
+const pageData = data.content.contents[4];
 
 it('renders', () => {
     const video = shallow(<Video {...pageData} toggleNavHide={() => {}} />);
