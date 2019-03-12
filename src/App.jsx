fd232816c00b7f6e8daea7c033aae2b26b97ec46
Brad <bradley.beatson@joipolloi.com>
2019-03-12 16:42:49 +0000

    change default timeout to two minutes

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ class App extends Component {
         this.onIdle = this.onIdle.bind(this);
 
         this.idleTimer = null;
-        this.idleTimeout = props.content.titles.idleTimeout || 6; // in seconds
+        this.idleTimeout = props.content.titles.idleTimeout || 120; // in seconds
     }
 
     onIdle(e) {
