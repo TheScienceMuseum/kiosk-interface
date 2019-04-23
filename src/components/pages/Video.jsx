c70798c9dae2942802c42e23fc829f1f3bc5f1ef
Brad <bradley.beatson@joipolloi.com>
2019-04-23 13:15:41 +0100

    bsl support

--- src/components/pages/Video.jsx
+++ src/components/pages/Video.jsx
@@ -23,6 +23,23 @@ class Video extends React.Component {
         this.onPause = this.onPause.bind(this);
         this.onPlay = this.onPlay.bind(this);
         this.handlePlayerState = this.handlePlayerState.bind(this);
+        this.handleBSL = this.handleBSL.bind(this);
+        this.handleSubtitles = this.handleSubtitles.bind(this);
+        this.getBslClass = this.getBslClass.bind(this);
+        this.getSubClass = this.getSubClass.bind(this);
+
+        this.state = {
+            showBSL: (!!(
+                typeof this.asset.bslSource !== 'undefined'
+                && this.asset.bslSource !== ''
+            )),
+            showSubtitles: (!!(
+                typeof this.asset.subtitlesSource !== 'undefined'
+                && this.asset.subtitlesSource !== ''
+            )),
+            bslEnabled: false,
+            subtitlesEnabled: false,
+        };
     }
 
     componentDidMount() {
@@ -39,6 +56,55 @@ class Video extends React.Component {
         pauseTimer();
     }
 
+    getBslClass() {
+        const { bslEnabled } = this.state;
+        if (bslEnabled) {
+            return 'enabled';
+        }
+        return '';
+    }
+
+    getSubClass() {
+        const { subtitlesEnabled } = this.state;
+        if (subtitlesEnabled) {
+            return 'enabled';
+        }
+        return '';
+    }
+
+    getSubTrack() {
+        return {
+            type: 'title',
+            value: this.asset.subtitlesSource,
+        };
+    }
+
+    handleBSL() {
+        const { bslEnabled } = this.state;
+
+        // get current play time
+        const playerState = this.player.getSate();
+
+        if (bslEnabled) {
+            // disable
+            this.player.load(this.asset.assetSource);
+            this.player.seek(playerState.currentTime);
+            this.setState({ bslEnabled: false });
+            return;
+        }
+
+        // enable & change source
+        this.player.load(this.asset.bslSource);
+        this.player.seek(playerState.currentTime);
+        this.setState({ bslEnabled: true });
+    }
+
+    handleSubtitles() {
+        const { subtitlesEnabled } = this.state;
+
+        this.setState({ subtitlesEnabled: !subtitlesEnabled });
+    }
+
     handlePlayerState(state, prevState) {
         if (prevState.paused !== state.paused) {
             if (!state.paused) {
@@ -50,17 +116,26 @@ class Video extends React.Component {
     }
 
     render() {
+        const { showBSL, showSubtitles } = this.state;
         return (
             <div className="Page PageVideo">
-                <Player ref={(node) => { this.player = node; }} autoPlay>
+                <Player ref={(node) => { this.player = node; }} autoPlay selectedTextTrack={this.getSubTrack()}>
                     <source src={this.asset.assetSource} type="video/mp4" />
                     <ControlBar autoHide={false} className="kioskControlBar">
-                        <button type="button" className="kioskPlayerControl end subtitles">
-                            Enable/Disable subtitles
-                        </button>
-                        <button type="button" className="kioskPlayerControl end bsl">
-                            Enable/Disable BSL
-                        </button>
+                        {
+                            showBSL && (
+                                <button type="button" className={`kioskPlayerControl end subtitles ${this.getBslClass()}`} onClick={this.handleBSL}>
+                                    Enable/Disable subtitles
+                                </button>
+                            )
+                        }
+                        {
+                            showSubtitles && (
+                                <button type="button" className={`kioskPlayerControl end bsl ${this.getSubClass()}`} onClick={this.handleSubtitles}>
+                                    Enable/Disable BSL
+                                </button>
+                            )
+                        }
                         <button
                             type="button"
                             className="kioskPlayerControl end close"
