ac3c512012f84b4344d8f79212ab27443035da6e
Lawrence <lawrence@joipolloi.com>
2019-06-07 15:38:46 +0100

    bump zoom factor

--- src/components/pages/Model/OrbitController.js
+++ src/components/pages/Model/OrbitController.js
@@ -53,7 +53,7 @@ module.exports = (THREE) => {
         // This option actually enables dollying in and out; left as "zoom" for backwards compatibility.
         // Set to false to disable zooming
         this.enableZoom = true;
-        this.zoomSpeed = 1.0;
+        this.zoomSpeed = 2.0;
 
         // Set to false to disable rotating
         this.enableRotate = true;
