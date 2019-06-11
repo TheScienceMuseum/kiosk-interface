2cd83a36205fe0886fa140d755f070a894bad20c
Lawrence <lawrence@joipolloi.com>
2019-06-11 10:03:26 +0100

    fix missing aspect prop on Menu

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -58,12 +58,14 @@ export const Dimensions = {
         MENU_ITEM_SPACING: 20,
         MENU_ITEM_PADDING: 100,
         TITLE_ITEM_WIDTH: 800,
+        ASPECT_RATIO: '16:9',
     },
     portrait: {
         MENU_ITEM_WIDTH: 800,
         MENU_ITEM_SPACING: 20,
         MENU_ITEM_PADDING: 100,
         TITLE_ITEM_WIDTH: 800,
+        ASPECT_RATIO: '9:16',
     },
 };
 
