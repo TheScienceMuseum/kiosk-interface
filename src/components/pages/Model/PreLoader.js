467dfa4a15889267825d5fd20127ed2e84da094f
Lawrence <lawrence@simplecat.software>
2019-08-24 19:36:59 +0100

    Integrate new model with corrected focus points

--- src/components/pages/Model/PreLoader.js
+++ src/components/pages/Model/PreLoader.js
@@ -6,7 +6,7 @@ import OBJLoader from './OBJLoader';
 import MTLLoader from './MTLLoader';
 import { ArticleTypes } from '../../../utils/Constants';
 
-if (!has(window, 'ModelStore')) {
+if (has(window, 'ModelStore')) {
     set(window, 'ModelStore', {});
 }
 
@@ -23,68 +23,32 @@ export default {
             }
             const { asset } = article;
 
-            const loader = new THREE.TextureLoader();
-            loader.setPath(asset.assetDirectory);
-            loader.load(
-                asset.texture,
-                (texture) => {
-                    const material = new THREE.MeshBasicMaterial({
-                        map: texture,
-                    });
-                    const objLoader = new THREE.OBJLoader();
+            const mtlLoader = new THREE.MTLLoader();
 
-                    objLoader.setPath(asset.assetDirectory);
-                    objLoader.load(
-                        // resource URL
-                        asset.obj,
-                        // called when resource is loaded
-                        (object) => {
-                            object.traverse((node) => {
-                                if (node.isMesh) {
-                                    // eslint-disable-next-line no-param-reassign
-                                    node.material = material;
-                                }
-                            });
-                            set(window, `ModelStore.${article.articleID}`, { asset, object });
-                        },
-                        // called when loading is in progresses
-                        (xhr) => {
-                            console.log(`${xhr.loaded / xhr.total * 100}% loaded`);
-                        },
-                        // called when loading has errors
-                        (error) => {
-                            console.error(`An error happened: ${error.toString()}`);
-                        },
-                    );
-                },
-            );
+            mtlLoader.setPath(asset.assetDirectory);
+            mtlLoader.load(asset.mtl, (materials) => {
+                materials.preload();
+                const objLoader = new THREE.OBJLoader();
 
-            // const mtlLoader = new THREE.MTLLoader();
-            //
-            // mtlLoader.setPath(asset.assetDirectory);
-            // mtlLoader.load(asset.mtl, (materials) => {
-            //     materials.preload();
-            //     const objLoader = new THREE.OBJLoader();
-            //
-            //     objLoader.setMaterials(materials);
-            //     objLoader.setPath(asset.assetDirectory);
-            //     objLoader.load(
-            //         // resource URL
-            //         asset.obj,
-            //         // called when resource is loaded
-            //         (object) => {
-            //             set(window, `ModelStore.${article.articleID}`, { asset, object });
-            //         },
-            //         // called when loading is in progresses
-            //         (xhr) => {
-            //             console.log(`${xhr.loaded / xhr.total * 100}% loaded`);
-            //         },
-            //         // called when loading has errors
-            //         (error) => {
-            //             console.error(`An error happened: ${error.toString()}`);
-            //         },
-            //     );
-            // });
+                objLoader.setMaterials(materials);
+                objLoader.setPath(asset.assetDirectory);
+                objLoader.load(
+                    // resource URL
+                    asset.obj,
+                    // called when resource is loaded
+                    (object) => {
+                        set(window, `ModelStore.${article.articleID}`, { asset, object });
+                    },
+                    // called when loading is in progresses
+                    (xhr) => {
+                        console.log(`${xhr.loaded / xhr.total * 100}% loaded`);
+                    },
+                    // called when loading has errors
+                    (error) => {
+                        console.error(`An error happened: ${error.toString()}`);
+                    },
+                );
+            });
         });
     },
     getModel: modelName => get(window, `ModelStore.${modelName}`, null),
